d4d69fa23e745b2a2879dec01b2531f2b79e9067
Dave <dave.cadelina@outlook.com>
2021-01-03 21:53:51 +1100

    homepage with hover effects completed

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({id, title, imageUrl, linkUrl}) => (
-    <div className="menu-item">
-        <div className="menu-item-content">
-            <h1 className="title">{title}</h1>
-            <p>SHOP NOW</p>
+const MenuItem = ({id, title, imageUrl, linkUrl}) => {
+
+    
+    return (
+        <div className="menu-item">
+            <div className="background-img" style={{backgroundImage: `url("${imageUrl}")`}}></div>
+            <div className="menu-item-content">
+                <h1 className="title">{title}</h1>
+                <p>SHOP NOW</p>
+            </div>
         </div>
-    </div>
-)
+    )
+}
+
 
 
 export default MenuItem;
