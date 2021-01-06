85fc4a387eef8fe69e14590db2a89a49f3da305d
Dave <dave.cadelina@outlook.com>
2021-01-07 00:50:05 +1100

    added base styling for collections

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import './collection-preview.styles.scss'
 
 const CollectionPreview = ({title, items}) => {
     return (
-        items.map((item, index)=> (
-            <div>
-                <div>{item.name}</div>
-                <img src={item.imageUrl} alt=""/>
+        <Fragment>
+            <div className="collection-preview">
+                <h1 className='title'>{title}</h1>
+                <div className="collection-items">
+                    {
+                        items.map((item, index)=> (
+                            <div className="collection-item">
+                                <img src={item.imageUrl} alt=""/>
+                                <div className="item-name">{item.name}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+
             </div>
-        ))
+
+        </Fragment>
+
     )
 }
 
