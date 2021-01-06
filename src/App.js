6f1e6cfc539ffec4acf6c68f66cb0d588f607812
Dave <dave.cadelina@outlook.com>
2021-01-06 20:25:34 +1100

    added shop data and collections component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.scss';
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
-
-  const HatsPage = () => {
-    return <h1>HATS PAGE</h1>
-  }
 
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
