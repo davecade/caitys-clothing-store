c01c23d38f3fc4f8c34af6c5662c7b317ccdfbf9
Dave <dave.cadelina@outlook.com>
2021-01-10 23:28:11 +1100

    made image object fit fill

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,15 +2,13 @@ import React, { Fragment } from 'react'
 import './collection-item.styles.scss'
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
-    <Fragment>
-        <div className="collection-item">
-            <img src={imageUrl} alt=""/>
-            <div className="item-details">
-                <p>{name}</p>
-                <p>{price}</p>
-            </div>
+    <div className="collection-item" >
+        <img className="image" src={imageUrl} alt=""/>
+        <div className="item-details">
+            <p>{name}</p>
+            <p>{price}</p>
         </div>
-    </Fragment>
+    </div>
 )
 
 export default CollectionItem
