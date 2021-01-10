9085bc06dac5c03d808b5e11d8c65c2c7a5d4a95
Dave <dave.cadelina@outlook.com>
2021-01-11 00:40:22 +1100

    added header or navbar with caity logo and nav options

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import { Route, Switch } from 'react-router-dom'
+import Header from './components/header/header.component'
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
