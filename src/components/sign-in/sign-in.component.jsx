a97eef6cf19bc59c8fd057dcf47a2e895ca2ade0
Dave <dave.cadelina@outlook.com>
2021-01-12 23:08:14 +1100

    added signin pages and sign in component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import './sign-in.styles.scss'
+
+class SignIn extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I aleady have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form action="">
+
+                    
+                </form>
+            </div>
+        )
+    }
+}
