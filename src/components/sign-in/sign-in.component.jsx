71fa97721f9f2b3b99dbd2be72edf8ecc62a5984
Dave <dave.cadelina@outlook.com>
2021-01-13 23:22:37 +1100

    added basic form fields

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -19,8 +19,12 @@ class SignIn extends Component {
                 <span>Sign in with your email and password</span>
 
                 <form action="">
+                    <input type="email" name="email" value={this.state.email} required />
+                    <label>Email</label>
+                    <input type="password" name="password" value={this.state.password} required />
+                    <label>Password</label>
 
-                    
+                    <input type="submit" value="Submit Form"/>
                 </form>
             </div>
         )
